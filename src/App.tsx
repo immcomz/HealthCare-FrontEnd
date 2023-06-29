7d69a98c75ba07bd95261acc2d3f4091efd86a2c
“Iranga <im@Irangas-MacBook-Pro.local>
2023-06-29 13:02:05 +1000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+
+interface Patient {
+  id: number;
+  name: string;
+  age: number;
+}
+const apiEndPoint = "http://16.50.98.184:3000/api/patients";
+function App() {
+  const [patients, setPatients] = useState<Patient[]>([]);
+
+  useEffect(() => {
+    fetchPatients();
+  }, []);
+
+  const fetchPatients = async () => {
+    try {
+      const response = await fetch(apiEndPoint);
+      const data = await response.json();
+      setPatients(data);
+    } catch (error) {
+      console.error("Error fetching patients:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Healthcare App</h1>
+      <ul>
+        {patients.map((patient) => (
+          <li key={patient.id}>
+            {patient.name} – {patient.age} years old
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
